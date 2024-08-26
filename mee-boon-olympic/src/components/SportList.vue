5e5e1f51072f28b01072ecd200a12e4159ddcf1e
Wasan Renumat <wasanrenumat@Wasans-MacBook-Pro.local>
2024-08-26 20:21:44 +0700

    SportList componet
    SportList component have been created

---
+++ mee-boon-olympic/src/components/SportList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <div v-for="sport in sports" :key="sport.sportName" class="bg-white p-3 rounded shadow-sm">
+      <h3 class="font-semibold text-gray-700">{{ sport.sportName }}</h3>
+      <p class="text-sm text-gray-600">Medals: {{ sport.medalsUrl }}</p>
+      <p class="text-sm text-gray-600">Rank: {{ sport.rank }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { type Detail } from '@/types';
+
+const props = defineProps<{ sports: Detail[] }>();
+</script>
