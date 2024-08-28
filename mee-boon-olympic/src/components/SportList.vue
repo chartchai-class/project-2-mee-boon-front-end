2a6da6f4f3ffdb8eec4a3484d0ead8813c8cde54
woraphon.p <woraphon_ph@cmu.ac.th>
2024-08-28 16:34:44 +0700

    update

--- mee-boon-olympic/src/components/SportList.vue
+++ mee-boon-olympic/src/components/SportList.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
+import { RouterLink, useRoute } from 'vue-router';
 import EventService from '@/services/EventService';
 import { type Detail } from '@/types';
 
@@ -22,25 +22,23 @@ onMounted(() => {
 
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-    <div
-      v-for="sport in sports"
-      :key="sport.sportName"
-      class="bg-gradient-to-r from-indigo-500 to-purple-600 text-white p-5 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 transform hover:-translate-y-1 hover:scale-105"
-    >
+    <div v-for="sport in sports" :key="sport.sportName"
+      class="bg-gradient-to-r from-indigo-500 to-purple-600 text-white p-5 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 transform hover:-translate-y-1 hover:scale-105">
       <div class="flex items-center justify-between">
         <h3 class="font-bold text-xl">{{ sport.sportName }}</h3>
-        <img :src="sport.medalsUrl" alt="Medal Icon" class="w-10 h-10 rounded-full border border-white" />
       </div>
       <p class="mt-3 text-sm">
         <span class="font-semibold">Rank:</span> {{ sport.rank }}
       </p>
       <div class="mt-4">
-        <button
-          class="w-full bg-white text-indigo-600 font-semibold py-2 px-4 rounded-lg hover:bg-gray-100 transition duration-300"
-        >
-          View Details
-        </button>
+        <RouterLink :to="{ name: 'about' }">
+          <button
+            class="w-full bg-white text-indigo-600 font-semibold py-2 px-4 rounded-lg hover:bg-gray-100 transition duration-300">
+            Cheer for Athlete
+          </button>
+        </RouterLink>
       </div>
+
     </div>
   </div>
 </template>
